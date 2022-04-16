efdb5cce468cec9457beb47c7a56138096a6cd42
diego <defsdiego@gmail.com>
2022-04-16 12:10:42 -0700

    uninstalled pywinauto

--- bot_pwa.py
+++
@@ -1,8 +0,0 @@
-import pywinauto as pwa
-
-def main():
-    app = pwa.Application().connect(path='C:\\Users\\diego\\AppData\\Local\\Discord')
-    print(dir(app))
-
-if __name__ == '__main__':
-    main()
